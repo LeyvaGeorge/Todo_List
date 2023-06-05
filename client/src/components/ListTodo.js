1507f938c84816796f3ea5b5f6e2b7fc1d4efa71
George Leyva <leyvageorge@hotmail.com>
2023-06-05 14:13:57 -0700

    build the Delete button

---
+++ client/src/components/ListTodo.js
@@ -0,0 +1,60 @@
+import React, { Fragment, useEffect, useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function ListTodo() {
+    const [todos, setTodos] = useState([]);
+
+    //delete todo function
+    const deleteTodo = async (id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: 'DELETE'
+            });
+            setTodos(todos.filter(todo => todo.todo_id !== id))
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    const getTodos = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/todos")
+            const jsonData = await response.json()
+
+            setTodos(jsonData);
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
+    useEffect(() => {
+        getTodos();
+    }, []);
+
+
+    return (
+        <Fragment>
+            {todos.map(todo => (
+                <Card key={todo.todo_id} style={{ width: '18rem' }}>
+
+                    <Card.Body>
+                        <Card.Title>{todo.task}</Card.Title>
+                        <Card.Text>
+                            {todo.description}
+                        </Card.Text>
+                        <Button variant="success">Edit</Button>
+                        <Button 
+                            variant="danger" 
+                            onClick={()=> deleteTodo(todo.todo_id)}
+                        >Delete</Button>
+                    </Card.Body>
+                </Card>                
+            ))}
+
+
+        </Fragment>
+    )
+}
+
+export default ListTodo;
